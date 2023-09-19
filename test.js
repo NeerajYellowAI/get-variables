4e99e79231336c4665ea66414c5528f03cc77f29
Neeraj Walia <122222243+nwaliaez@users.noreply.github.com>
2023-09-19 23:56:04 +0530

    test

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const test = async () => {
+    console.log('ghj');
     const res = await fetch(
         'https://r4.cloud.yellow.ai/api/ai/variables?bot=x1690175644644',
         {
@@ -6,7 +7,6 @@ const test = async () => {
             headers: {
                 accept: 'application/json',
                 'x-api-key': 'hI0uyyZNKqJIRQsI0carq4Q3J-j9Jy3ie_mE-2L7',
-                cookie: 'ym_xid=fa5329026bc349d7646a38bd0e05fb389764759e744acc923e3a6e13bdcd0b2dvxRTGEW5IWEHausIiCG7u',
             },
         }
     );
