661e87b612981e5132b868bd82fd5feba40b32f8
Neeraj Walia <122222243+nwaliaez@users.noreply.github.com>
2023-09-19 23:51:39 +0530

    test

--- test.js
+++ test.js
@@ -4,6 +4,8 @@ const test = async () => {
         {
             method: 'GET',
             headers: {
+                accept: 'application/json',
+                'x-api-key': 'hI0uyyZNKqJIRQsI0carq4Q3J-j9Jy3ie_mE-2L7',
                 cookie: 'ym_xid=fa5329026bc349d7646a38bd0e05fb389764759e744acc923e3a6e13bdcd0b2dvxRTGEW5IWEHausIiCG7u',
             },
         }
